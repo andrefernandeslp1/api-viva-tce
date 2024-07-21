d8bbe8c80a9661dde8fd60bed38ea799c3a33e58
felipemadu13 <felipemadureira.a@hotmail.com>
2024-07-21 16:51:29 -0300

    Fix: Descrição das mudanças

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76001798cee818002f93d032cc2e3710083bd57b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2bb69c4134b40cf1396ac27660e5a33dcba7d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
